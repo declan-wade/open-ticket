ce6e8432986f564a7594193d4f1657fd3c0ce20f
Declan Wade <17171018+declan-wade@users.noreply.github.com>
2024-06-25 11:45:04 +0800

    Added statement for updating user

--- app/api/newUser/route.ts
+++ app/api/newUser/route.ts
@@ -1,22 +1,15 @@
 "use server";
 
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function POST(req: Request, res: Response) {
-  const { data }: any = await req.json();
+  const { data, type }: any = await req.json();
+  if (type === "user.created") {
     try {
-      console.log(data)
-/*       const {
-        id,
-        email_addresses,
-        first_name,
-        last_name,
-        primary_email_address_id,
-      } = data; */
-      console.log("running prisma")
+      console.log("running prisma");
       const user = await prisma.user.create({
         data: {
           id: data.id,
@@ -26,11 +19,38 @@ export async function POST(req: Request, res: Response) {
         } as any,
       });
 
-      return NextResponse.json({ user }, {status: 201});
+      return NextResponse.json({ user }, { status: 201 });
     } catch (error) {
       console.error("Error creating user:", error);
-      return NextResponse.json({ error: "Internal Server Error" }, {status: 500});
+      return NextResponse.json(
+        { error: "Internal Server Error" },
+        { status: 500 }
+      );
     } finally {
       await prisma.$disconnect();
     }
-  } 
+  } else {
+    try {
+      console.log("running prisma");
+      const user = await prisma.user.update({
+        where: { id: data.id },
+        data: {
+          id: data.id,
+          last_name: data.last_name,
+          first_name: data.first_name,
+          email: data.email_addresses[0].email_address,
+        } as any,
+      });
+
+      return NextResponse.json({ user }, { status: 201 });
+    } catch (error) {
+      console.error("Error creating user:", error);
+      return NextResponse.json(
+        { error: "Internal Server Error" },
+        { status: 500 }
+      );
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+}
